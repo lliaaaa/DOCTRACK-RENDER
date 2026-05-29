b6e79b070be90697e1300eeb1c3a84c583c6e563
Larraine <larrainenataliacamposano@gmail.com>
2026-05-29 11:25:16 +0800

    Add temp_admin_expires_at and must_change_password fields
    Add columns for temporary admin expiry and password change requirement.

--- migrations/versions/add_temp_admin_expiry_and_must_change_password.py
+++ migrations/versions/add_temp_admin_expiry_and_must_change_password.py
@@ -1,7 +1,7 @@
 """Add temp_admin_expires_at and must_change_password to accounts
 
 Revision ID: a1b2c3d4e5f6
-Revises: merge_all_heads
+Revises: merge_all_heads_001
 Create Date: 2026-05-29
 
 Fixes:
@@ -12,7 +12,7 @@ from alembic import op
 import sqlalchemy as sa
 
 revision = 'a1b2c3d4e5f6'
-down_revision = 'merge_all_heads'
+down_revision = 'merge_all_heads_001'
 branch_labels = None
 depends_on = None
 
@@ -30,3 +30,4 @@ def upgrade():
 def downgrade():
     op.drop_column('accounts', 'must_change_password')
     op.drop_column('accounts', 'temp_admin_expires_at')
+  
